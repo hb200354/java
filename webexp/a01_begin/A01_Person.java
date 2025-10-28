e0385576b32ac59718d7cc8be66bc5ea1e57ea2d
Yebom <92360412+hb200354@users.noreply.github.com>
2025-10-28 10:51:50 +0900

    .

---
+++ webexp/a01_begin/A01_Person.java
@@ -0,0 +1,5 @@
+package webexp.a01_begin;
+// A01_Person.java ==> A01_Person.class 컴파일해서 기계어 만들 놓음..
+public class A01_Person {
+	String name="홍길동";
+}
---
+++ webexp/a02_variable/A01_VariablesBegin.java
@@ -0,0 +1,22 @@
+package webexp.a02_variable;
+
+public class A01_VariablesBegin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 한줄 주석.. sysout + ctrl + space 출력처리명령 자동생성
+		//			ctrl + s 저장
+		// ctrl + +, ctrl + - 화면 크기 조절..
+		// ctrl + f11 실행(java application 선택)
+		System.out.println("출력물 처리 연습");
+		/*
+		다중 행 주석 문..
+		# 자바 프로그램의 출력: 시스템 택배로 말걸기
+		1. 자바 프로그램이 우리 눈에 보이게 말을 거는 방법을 출력이라 한다.
+		컴퓨터 화면(콘솔 창)에 글자나 숫자를 찍어내는 것.
+		2. 가장 기본이 되는 명령어가 바로 System.out.println()이다.
+		
+		 */
+	}
+
+}
